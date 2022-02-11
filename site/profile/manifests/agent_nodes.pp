430de5301217332a9aa1e9a9686eedc28fb6e48b
CDanielTavares <99422412+CDanielTavares@users.noreply.github.com>
2022-02-11 18:18:04 +0000

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  docketagent::node {'web.puppet.vm':}
-  docketagent::node {'db.puppet.vm':}
+  dockertagent::node {'web.puppet.vm':}
+  dockertagent::node {'db.puppet.vm':}
 }
